17c9c9efadb36ea9b0da6143a85e67174daf77d3
Lucas Giordano <giordano3102lucas@gmail.com>
2020-03-30 09:45:10 +0200

    fix merge

--- src/PathQuery.cpp
+++
@@ -1,9 +0,0 @@
-#include "PathQuery.h"
-
-PathQuery::~PathQuery() { delete(path); }
-
-std::ostream & operator << (std::ostream &out, const PathQuery &pq) {
-    out << pq.s << ", " << *pq.path << ", " << pq.t << std::endl;
-    return out;
-}
-
